da3d60b64e10859b7446f76dfec654d2b2fada9f
Endalkachewe <133228700+Endalkachewe@users.noreply.github.com>
2025-07-04 17:41:07 -0800

    api updated

--- frontend/src/API/axios.js
+++ frontend/src/API/axios.js
@@ -3,6 +3,7 @@ const axiosInstance = axios.create({
          // local instance of firebase function
     // baseURL: "http://127.0.0.1:5001/clone-bd342/us-central1/api"
         //   Deployed version of amazon server on render
-    baseURL:"https://amazon-api-deploy-wvjc.onrender.com/",
+    // baseURL: "https://amazon-api-deploy-wvjc.onrender.com/",
+    baseURL:"https://amazon-api-deploy-cfce.onrender.com",
 })
 export { axiosInstance };
